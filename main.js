29658fd8141b9c46454e4562014bc70a4132c9ea
Simon Olsen <simonolsen@Simons-MacBook-Pro.local>
2019-09-17 16:58:10 -0700

    hardcoded the 2 books because i will not have a library.

---
+++ main.js
@@ -0,0 +1,34 @@
+// Book Class: Represents a book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+//  UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const storedBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "3434434"
+      },
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "3434434"
+      }
+    ];
+  }
+}
+
+// Store Class: Handles Storage
+
+// Events: Display Books
+
+// Event: Add a book
+
+// Event: Remove book
