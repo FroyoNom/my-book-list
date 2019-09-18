9ffdf639aa6565c1ec75796d2b6e3209fe4ae34b
Simon Olsen <simonolsen@Simons-MacBook-Pro.local>
2019-09-17 17:06:10 -0700

    added event listener to display the books in the table.

--- main.js
+++ main.js
@@ -22,12 +22,32 @@ class UI {
         isbn: "3434434"
       }
     ];
+
+    const books = storedBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
   }
 }
 
 // Store Class: Handles Storage
 
 // Events: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a book
 
